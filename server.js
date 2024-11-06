abb73bfad374cab30b70673a1af39e58630cbc0a
Rahu1Singh <srahul1357@gmail.com>
2024-11-06 13:20:05 +0530

    Updated files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Bye from Node.js App!');
+  res.send('Byee from Node.js App!');
 });
 
 app.listen(PORT, () => {
